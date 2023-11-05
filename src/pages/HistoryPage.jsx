74c31c6da671c6f164d7c4c89b5c92f24cc49ec5
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-11-05 21:43:28 +0530

    api fetch of history page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -33,7 +33,7 @@ const History = () => {
   
 
   return (
-    <div className='h-screen w-screen flex flex-col mt-20 text-white'>History</div>
+    <div className='h-screen w-screen flex flex-col mt-20 text-white'>History ghiougiyfgiyf</div>
   )
 }
 
